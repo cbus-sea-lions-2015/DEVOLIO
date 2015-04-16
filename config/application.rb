9a028a4ce1814aec4118c91c72508bdf930f7402
Chris Singer <ccsinger@gmail.com>
2015-04-16 17:42:29 -0400

    Add rspec test suite with CI

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module DEVOLIO
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    
+    config.autoload_paths += %W(#{config.root}/lib) # add this line
   end
 end
