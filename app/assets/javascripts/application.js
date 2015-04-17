8f1c8705265441470f6d8dc3a4753a2b008cf551
Jan Dennison <jldennison@gmail.com>
2015-04-17 11:39:16 -0400

    add backbone to app.js, remove turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require underscore
+//= require backbone
+
+//= require_tree ./templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
 //= require_tree .
