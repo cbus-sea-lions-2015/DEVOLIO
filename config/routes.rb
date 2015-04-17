a4471ca83f650d56ea499d630b3a5eca73f840db
Chris Singer <ccsinger@gmail.com>
2015-04-17 16:48:58 -0400

    i

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
-  get 'run_api', to: 'api#run'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-=======
->>>>>>> master
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:show, :edit, :update, :destroy]
+  
+  get 'run_api', to: 'api#run'
 
   root to: 'static#index'
 end
