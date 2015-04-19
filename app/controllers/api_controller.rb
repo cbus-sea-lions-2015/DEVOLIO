18d615c0c0b4840003dc5e2a2404a4beec8e0eef
Mary Baylis <mary.c.baylis@gmail.com>
2015-04-19 00:55:48 -0400

    Add github action to API controller.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,4 +3,14 @@ class ApiController < ApplicationController
     ApiRunner.new(current_user)
     redirect_to root_path
   end
+
+  def github
+    current_user.github_profile.update_attributes(github_params)
+    redirect_to root_path
+  end
+
+  private 
+  def github_params
+    params.require(:github_profile).permit(:pushEvents, :pullEvents, :gitsEvents, :forkEvents, :mostRecentEventDate, :oldestEventDate, :commits, :lineAdditions, :lineDeletions, :commitMessages, :languages, :username, :fullName, :aviurl, :profileurl, :location, :followersnum, :followingnum, :reposnum)
+  end
 end
