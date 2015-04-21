836b1bd6659831a58119698c814599cfc1c66045
Jan Dennison <jldennison@gmail.com>
2015-04-21 16:02:53 -0400

    position history

--- app/assets/javascripts/formHelper.js
+++ app/assets/javascripts/formHelper.js
@@ -1,35 +1,16 @@
 // Add employment positions
 function addEmploymentPosition() {
-  var i = 1;
   $(document).on('click','.add-position', function(e){
     e.preventDefault();
+    var i = $('.added-position').length + 1;
     var sibling = $(this).siblings(".skill-position"),
         parent = $(this).parent('fieldset');
-    var positionForm = '<div class="position col-xs-12">
-        <fieldset>
-          <label for="position" class="col-xs-12 col-sm-3">Position Title</label>
-          <input class="col-xs-12 col-sm-6" type="text" name="positions[' + i + '][title]">
-        </fieldset>
-        <fieldset>
-          <label for="company" class="col-xs-12 col-sm-3">Company</label>
-          <input class="col-xs-12 col-sm-6" type="text" name="positions[' + i + '][company]">
-        </fieldset>
-        <fieldset>
-          <label for="time" class="col-xs-12 col-sm-3">Date Range</label>
-          <input class="col-xs-12 col-sm-6" type="text" name="positions[' + i + '][range]">
-        </fieldset>
-        <fieldset>
-          <label for="position" class="col-xs-12 col-sm-3">Short Description</label>
-          <input class="col-xs-12 col-sm-6" type="text" name="positions[' + i + '][description]">
-        </fieldset>
-      </div>';
+    var positionForm = '<div class="added-position col-xs-12"><fieldset><label for="position" class="col-xs-12 col-sm-3">Position Title</label><input class="col-xs-12 col-sm-6" type="text" name="positions[' + i + '][title]"></fieldset><fieldset><label for="company" class="col-xs-12 col-sm-3">Company</label><input class="col-xs-12 col-sm-6" type="text" name="positions[' + i + '][company]"></fieldset><fieldset><label for="time" class="col-xs-12 col-sm-3">Date Range</label><input class="col-xs-12 col-sm-6" type="text" name="positions[' + i + '][range]"></fieldset><fieldset><label for="position" class="col-xs-12 col-sm-3">Short Description</label><input class="col-xs-12 col-sm-6" type="text" name="positions[' + i + '][description]"></fieldset></div>';
     if( i > 3 ) {
       parent.find('.error').removeClass('hidden');
     } else {
       parent.find('.error').addClass('hidden');
-      parent.append(skillMeter);
-      sibling.val('');
-      i += 1;
+      parent.append(positionForm);
     }
   });
 }
