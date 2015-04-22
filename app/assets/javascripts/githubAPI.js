e3465841ea8cd825a2a54eaed302ea47c7d664d9
Jan Dennison <jldennison@gmail.com>
2015-04-22 11:01:31 -0400

    refactor variable structure

--- app/assets/javascripts/githubAPI.js
+++ app/assets/javascripts/githubAPI.js
@@ -1,8 +1,6 @@
 var getGitHubData = function(username){
-  var githubResults = {}
-  var user_uri   = 'https://api.github.com/users/'+username;
-  var events_uri = 'https://api.github.com/users/'+username+'/events';
-  var repos_uri = 'https://api.github.com/users/'+username+'/repos';
+  var githubResults = {},
+      api_uri = 'https://api.github.com/users/';
 
   // var test;
   requestJSON(user_uri, function(json){
@@ -68,49 +66,83 @@ var getGitHubData = function(username){
         mostRecentEventDate = events[0].created_at;
         oldestEventDate = events[events.length-1].created_at;
 
-        githubResults.pushEvents = pushEvents;
-        githubResults.pullEvents = pullEvents;
-        githubResults.gistEvents = gistEvents;
-        githubResults.forkEvents = forkEvents;
-        githubResults.eventDates = eventDates
-        githubResults.mostRecentEventDate = mostRecentEventDate;
-        githubResults.oldestEventDate = oldestEventDate;
-        githubResults.commits = commits;
-        githubResults.lineAdditions = additions;
-        githubResults.lineDeletions = deletions;
-        githubResults.commitMessages = commitMessages;
-        githubResults.languages = languages;
+  function extractBasicResults(data) {
+    githubResults.github_handle = data.login;
+    githubResults.fullName = data.name || githubResults.username;
+    githubResults.aviurl = data.avatar_url;
+    githubResults.profileurl = data.html_url;
+    githubResults.location = data.location;
+    githubResults.followersnum = data.followers;
+    githubResults.followingnum = data.following;
+    githubResults.reposnum = data.public_repos;
+    githubResults.reposData = extractReposData(data);
+  }
 
-        var repos;
-        var allLang = {}
-        var allStars = 0
-        var reposData = {}
-
-        $.getJSON(repos_uri, function(json){
-          repos = json;
-          for (var i = 0; i < repos.length; i++) {
-            allStars += repos[i].stargazers_count;
-            var rlanguage = repos[i].language;
-            if (rlanguage in allLang) {
-              allLang[rlanguage]++;
-            }
-            else {
-              allLang[rlanguage] = 1;
-            }
-          }
-          reposData.allLang = allLang;
-          reposData.allStars = allStars
-          githubResults.reposData = reposData
-          
-          console.log(githubResults.reposData)
-          console.log(githubResults)
-          saveGithubResults(githubResults);
-        })
-      })
+  function extractReposData(data) {
+    var repos = data;
+    var allStars = 0,
+        allLang = {},
+        reposData = {}
+    for (var i = 0; i < repos.length; i++) {
+      allStars += repos[i].stargazers_count;
+      var rlanguage = repos[i].language;
+      if (rlanguage in allLang) {
+        allLang[rlanguage]++;
+      }
+      else {
+        allLang[rlanguage] = 1;
+      }
     }
-  });
-}
-window.getGitHubData = getGitHubData;
+    reposData.allLang = allLang;
+    reposData.allStars = allStars
+    return reposData
+  }
+
+  function extractActivityResults(data) {
+    var events = data;
+    var commitMessages = [],
+        pushEvents = 0,
+        languages = {},
+        pullEvents = 0,
+        commits = 0,
+        additions = 0,
+        deletions = 0,
+        gistEvents = 0,
+        forkEvents = 0,
+        mostRecentEventDate,
+        oldestEventDate,
+        eventDates = [];
+    for (var i = 0; i < events.length; i++) {
+      if (events[i].type === "PushEvent") {
+        var commitsCollection = events[i].payload.commits
+        for (var j = 0; j < commitsCollection.length; j++){
+          commitMessages.push({message: commitsCollection[j].message, url: events[i].repo.url});
+        }
+        pushEvents += 1;
+      }
+      else if (events[i].type === "PullRequestEvent") {
+        var language = events[i].payload.pull_request.head.repo.language;
+        if (language in languages) {
+          languages[language]++;
+        }
+        else {
+          languages[language] = 1;
+        }
+        pullEvents += 1;
+        commits += events[i].payload.pull_request.commits;
+        additions += events[i].payload.pull_request.additions;
+        deletions += events[i].payload.pull_request.deletions;
+      }
+      else if (events[i].type === "GistEvent") {
+        gistEvents++;
+      }
+      else if (events[i].type === "ForkEvent") {
+        forkEvents++;
+      }
+      eventDates.push({date: events[i].created_at, eventType: events[i].type});
+    }
+    mostRecentEventDate = events[0].created_at;
+    oldestEventDate = events[events.length-1].created_at;
 
 function requestJSON(url, callback) {
   var otherTest = $.getJSON(url, {
