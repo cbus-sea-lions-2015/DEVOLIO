477cfd55deae48a2efbfe2ff9e29e43f79c9d7ae
Jan Dennison <jldennison@gmail.com>
2015-04-22 11:01:56 -0400

    refactor api calls

--- app/assets/javascripts/githubAPI.js
+++ app/assets/javascripts/githubAPI.js
@@ -2,69 +2,42 @@ var getGitHubData = function(username){
   var githubResults = {},
       api_uri = 'https://api.github.com/users/';
 
-  // var test;
-  requestJSON(user_uri, function(json){
-    if(json.message == "Not Found" || username == '') {
-      $('.message').html("<h2>No User Info Found</h2>");
-    }
-    else {
-      // else we have a user and we display their info
-      githubResults.github_handle = json.login;
-      githubResults.fullName = json.name || githubResults.username;
-      githubResults.aviurl = json.avatar_url;
-      githubResults.profileurl = json.html_url;
-      githubResults.location = json.location;
-      githubResults.followersnum = json.followers;
-      githubResults.followingnum = json.following;
-      githubResults.reposnum = json.public_repos;
+  // First API call: user
+  requestJSON(api_uri + username, function(userdata) {
+    // Second call: user repos
+    requestJSON(api_uri + username + '/repos', function(repodata) {
+      userdata["repos"] = repodata
+      requestUserActivity();
+      extractBasicResults(userdata);
+    });
+  });
 
-      var events;
-      var pushEvents = 0;
-      var pullEvents = 0;
-      var gistEvents = 0;
-      var forkEvents = 0;
-      var mostRecentEventDate;
-      var oldestEventDate;
-      var eventDates = [];
-      var languages = {};
-      var commits = 0;
-      var additions = 0;
-      var deletions = 0;
-      var commitMessages = [];
+  function requestUserActivity(){
+    // Third call: Activity page 1
+    requestJSON(api_uri + username + '/events?page=1', function(page1Data) {
+      // Fourth call: Activity page 2
+      requestJSON(api_uri + username + '/events?page=2', function(page2Data) {
+        // Fifth call: Activity page 3
+        requestJSON(api_uri + username + '/events?page=3', function(page3Data) {
+          extractActivityResults(page1Data.concat(page2Data,page3Data));
+          saveGithubResults(githubResults);
+        });
+      });
+    });
+  }
 
-      $.getJSON(events_uri, function(json){
-        events = json;
-        for (var i = 0; i < events.length; i++) {
-          if (events[i].type === "PushEvent") {
-            var commitsCollection = events[i].payload.commits
-            for (var j = 0; j < commitsCollection.length; j++){
-              commitMessages.push({message: commitsCollection[j].message, url: events[i].repo.url});
-            }
-            pushEvents += 1;
-          }
-          else if (events[i].type === "PullRequestEvent") {
-            var language = events[i].payload.pull_request.head.repo.language;
-            if (language in languages) {
-              languages[language]++;
-            }
-            else {
-              languages[language] = 1;
-            }
-            pullEvents += 1;
-            commits += events[i].payload.pull_request.commits;
-            additions += events[i].payload.pull_request.additions;
-            deletions += events[i].payload.pull_request.deletions;
-          }
-          else if (events[i].type === "GistEvent") {
-            gistEvents++;
-          }
-          else if (events[i].type === "ForkEvent") {
-            forkEvents++;
-          }
-          eventDates.push({date: events[i].created_at, eventType: events[i].type});
-        }
-        mostRecentEventDate = events[0].created_at;
-        oldestEventDate = events[events.length-1].created_at;
+  function requestJSON(url, callback) {
+    var request = $.getJSON(url, {
+      format: "json",
+    }).error(function(error) {
+      $('.message').html("<h2>"+ error +"</h2>");
+      console.log("API request error message: ",error);
+    }).done(function(data) {
+      console.log("API request success!",url)
+      callback.call(null, data);
+    });
+    return request;
+  }
 
   function extractBasicResults(data) {
     githubResults.github_handle = data.login;
@@ -144,22 +117,33 @@ var getGitHubData = function(username){
     mostRecentEventDate = events[0].created_at;
     oldestEventDate = events[events.length-1].created_at;
 
-function requestJSON(url, callback) {
-  var otherTest = $.getJSON(url, {
-    format: "json",
-  }).error(function(error) {
-    $('.message').html("<h2>"+ error +"</h2>");
-  }).done(function(data) {
-    callback.call(null, data);
-  });
-return otherTest;
+    githubResults.pushEvents = pushEvents;
+    githubResults.pullEvents = pullEvents;
+    githubResults.gistEvents = gistEvents;
+    githubResults.forkEvents = forkEvents;
+    githubResults.eventDates = eventDates;
+    githubResults.mostRecentEventDate = mostRecentEventDate;
+    githubResults.oldestEventDate = oldestEventDate;
+    githubResults.commits = commits;
+    githubResults.lineAdditions = additions;
+    githubResults.lineDeletions = deletions;
+    githubResults.commitMessages = commitMessages;
+    githubResults.languages = languages;
+  }
+
+  function saveGithubResults(githubResults) {
+    $.ajax({
+      url: '/store_github',
+      type: 'POST',
+      data: {github_profile: githubResults}
+    }).done(function(data){
+      $('.message').html("<h2>GitHub save success!</h2>");
+      console.log("GitHub save success!");
+    }).error(function(error) {
+      $('.message').html("<h2>"+ error +"</h2>");
+      console.log("Save error message: ",error);
+    });
+  }
 }
 
-function saveGithubResults(githubResults) {
-  $.ajax({
-    url: '/store_github',
-    type: 'POST',
-    data: {github_profile: githubResults}
-  }).done(function(data){
-  })
-}
+window.getGitHubData = getGitHubData;
